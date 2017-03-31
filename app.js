3131d731c3ef51ce73b35e4693c83d4c69143aba
FoxUSA <jake@jakelis.com>
2017-03-30 18:26:15 -0700

    bunch of stuff

--- app.js
+++ app.js
@@ -3,15 +3,33 @@ var http = require("http").Server(app);
 var io = require("socket.io")(http);
 var config = require("./config/config.js");
 var boardService = require("./lib/Board.Service.js");
+var uuid = require("node-uuid");
+
+var createRobber = function(pieces,tile){
+    var id = uuid.v4();
+    pieces[id]={
+        x:tile.x,
+        y:tile.y,
+        spriteKey: "robber",
+        id: id,
+        angle:0
+    };
+};
 
 //Setup board
     var tiles = [];
     var pieces={};
     boardService.createBoard(tiles);
-    boardService.createPiece(pieces, 800,275,100,0,0);
-    boardService.createPiece(pieces, 800,1050,100,0,1);
+    boardService.createPiece(pieces, 800,240,100,0,0);
+    boardService.createPiece(pieces, 800,1100,100,0,1);
     boardService.createPiece(pieces, 400,600,0,100,2);
     boardService.createPiece(pieces, 1300,600,0,100,3);
+
+
+    tiles.filter(function(tile){
+        if(tile.tileType=="desert")
+            createRobber(pieces,tile);
+    });
 
 //Status page
     app.get("/", function(req, res){
@@ -25,7 +43,7 @@ var boardService = require("./lib/Board.Service.js");
         console.log("User connected");
 
         //Request full board update
-            socket.on("requestInitBoardUpdate", function(data){
+            socket.on("requestInitBoardUpdate", function(){
                 //Send full board info
                 socket.emit("initBoardUpdate", {
                     tiles:tiles,
@@ -33,11 +51,17 @@ var boardService = require("./lib/Board.Service.js");
                 });
             });
 
+        //Roll
+            socket.on("roll", function(){
+                io.local.emit("roll", boardService.diceRoll());
+            });
+
         //Player piece is dragged
             socket.on("pieceUpdate", function(data){
                 console.log("pieceUpdate received");
                 pieces[data.id].x=data.x;
                 pieces[data.id].y=data.y;
+                pieces[data.id].angle=data.angle;
                 socket.broadcast.emit("pieceUpdate", data);
             });
     });
