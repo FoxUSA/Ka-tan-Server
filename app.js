eed47d93729e8018682671f4600a838639e674df
Matt <matthew.watts.mw@gmail.com>
2021-03-18 19:23:58 -0700

    Add eslint and update all code to abide by airbnb sytax rules

--- app.js
+++ app.js
@@ -1,73 +1,72 @@
-var app = require("express")();
-var http = require("http").Server(app);
-var io = require("socket.io")(http);
-var config = require("./config/config.js");
-var boardService = require("./lib/Board.Service.js");
-var uuid = require("node-uuid");
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const uuid = require('node-uuid');
+const config = require('./config/config.js');
+const boardService = require('./lib/Board.Service.js');
 
-var createRobber = function(pieces,tile){
-    var id = uuid.v4();
-    pieces[id]={
-        x:tile.x,
-        y:tile.y,
-        spriteKey: "robber",
-        id: id,
-        angle:0
-    };
+const createRobber = (pieces, tile) => {
+  const id = uuid.v4();
+  pieces[id] = {
+    x: tile.x,
+    y: tile.y,
+    spriteKey: 'robber',
+    id,
+    angle: 0,
+  };
 };
 
-//Setup board
-    var tiles = [];
-    var pieces={};
-    var docks = [];
-    boardService.creatDocks(docks);
-    boardService.createBoard(tiles);
-    boardService.createPiece(pieces, 800,240,100,0,0);
-    boardService.createPiece(pieces, 800,1150,100,0,1);
-    boardService.createPiece(pieces, 400,600,0,100,2);
-    boardService.createPiece(pieces, 1350,600,0,100,3);
+// Setup board
+const tiles = [];
+const pieces = {};
+const docks = [];
+boardService.createDocks(docks);
+boardService.createBoard(tiles);
+boardService.createPiece(pieces, 800, 240, 100, 0, 0);
+boardService.createPiece(pieces, 800, 1150, 100, 0, 1);
+boardService.createPiece(pieces, 400, 600, 0, 100, 2);
+boardService.createPiece(pieces, 1350, 600, 0, 100, 3);
 
+tiles.forEach((tile) => {
+  if (tile.tileType === 'desert') {
+    createRobber(pieces, tile);
+  }
+});
 
-    tiles.filter(function(tile){
-        if(tile.tileType=="desert")
-            createRobber(pieces,tile);
+// Status page
+app.get('/', (req, res) => {
+  res.status(200)
+    .json({ version: config.version })
+    .end();
+});
+
+// Socket connections
+io.on('connection', (socket) => {
+  // Request full board update
+  socket.on('requestInitBoardUpdate', () => {
+    // Send full board info
+    socket.emit('initBoardUpdate', {
+      tiles,
+      pieces,
+      docks,
     });
+  });
 
-//Status page
-    app.get("/", function(req, res){
-        res.status(200)
-                .json({version:config.version})
-                .end();
-    });
+  // Roll
+  socket.on('roll', () => {
+    io.local.emit('roll', boardService.diceRoll());
+  });
 
-//Socket connections
-    io.on("connection", function(socket){
-        console.log("User connected");
+  // Player piece is dragged
+  socket.on('pieceUpdate', (data) => {
+    pieces[data.id].x = data.x;
+    pieces[data.id].y = data.y;
+    pieces[data.id].angle = data.angle;
+    socket.broadcast.emit('pieceUpdate', data);
+  });
+});
 
-        //Request full board update
-            socket.on("requestInitBoardUpdate", function(){
-                //Send full board info
-                socket.emit("initBoardUpdate", {
-                    tiles:tiles,
-                    pieces:pieces,
-                    docks:docks
-                });
-            });
-
-        //Roll
-            socket.on("roll", function(){
-                io.local.emit("roll", boardService.diceRoll());
-            });
-
-        //Player piece is dragged
-            socket.on("pieceUpdate", function(data){
-                pieces[data.id].x=data.x;
-                pieces[data.id].y=data.y;
-                pieces[data.id].angle=data.angle;
-                socket.broadcast.emit("pieceUpdate", data);
-            });
-    });
-
-http.listen(config.port, function(){
-    console.log("listening on *:"+config.port);
+http.listen(config.port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`listening on *:${config.port}`);
 });
